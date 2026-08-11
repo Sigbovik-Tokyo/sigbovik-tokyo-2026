16f133d0b35a740e27ea323a44dd78362a42ac85
teddyboy999 <haribhushith9999@gmail.com>
2026-08-12 03:35:08 +0900

    finished base about us page

--- server/src/app.py
+++ server/src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for, render_template
 from typing import Callable, Any
 from sigtemplate.templite import Templite
 from anint import translations, Translator
@@ -21,6 +21,15 @@ t: Callable[[str, tuple[Any]], str] = translator.translate
 
 
 def render(locale: str, template_name: str, *contexts: Any) -> str:
+    # Load webpage images
+    webpage_images: list = [
+        # about us images
+        ("tsn_logo", url_for("static", filename="/assets/images/organizer_logos/TSN_logo_blue.png")),
+        ("tuj_cs_society_logo", url_for("static", filename="assets/images/organizer_logos/TUJ_CS_SOCIETY_LOGO.JPG")),
+        ("gdgoc_iput_logo", url_for("static", filename="/assets/images/organizer_logos/GDG-on-Campus-IPUT-Icon-Dark.png")),
+        ("gdgoc_waseda_logo", url_for("static", filename="/assets/images/organizer_logos/Copy of __ GDG + PA + Chapter Lockup - Centered - Template.png")),
+    ]
+
     try:
         translator.set_locale(locale)
         with open(f"templates/{template_name}.html", "r") as f:
@@ -28,7 +37,10 @@ def render(locale: str, template_name: str, *contexts: Any) -> str:
             return Templite(
                 html_string,
                 *[dict(map(lambda kv: (kv[0], t(kv[1])), context.items()))
-                    for context in contexts]
+                    for context in contexts],
+                *[
+                    dict(map(lambda image_link: (image_link[0], image_link[1]), webpage_images))
+                ] # passing images as context
                 ).render()
     except Exception as e:
         print(f"ERROR: {str(e)}")
@@ -45,6 +57,16 @@ def render_index(locale: str) -> str:
     )
 
 
+def render_about(locale: str) -> str:
+    return render(
+        locale,
+        "about",
+        {"lang": "index.lang"},
+        {"title": "index.title"},
+        {"body": "index.body"},
+    )
+
+
 @app.route("/")
 def index_en():
     return render_index("en")
@@ -53,3 +75,8 @@ def index_en():
 @app.route("/ja/")
 def index_ja():
     return render_index("ja")
+
+
+@app.route("/about")
+def about_en():
+    return render_about("en")
